344deddfb33013fd0e634e4290dac0976a5b0b18
Sandeep kumar <sandeepkarmkar49@gmail.com>
2021-06-21 11:05:48 +0530

    hello

--- ui_landing_page.py
+++ ui_landing_page.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 ################################################################################
-## Form generated from reading UI file 'ui_landing_pageZRwHtw.ui'
+## Form generated from reading UI file 'ui_landing_pagetxOcWr.ui'
 ##
 ## Created by: Qt User Interface Compiler version 5.14.1
 ##
